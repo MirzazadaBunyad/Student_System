21f32ebd2a9a3607fc38c5734e18844bb37140ad
Mirzazada Bunyad <115315853+MirzazadaBunyad@users.noreply.github.com>
2024-04-15 20:00:43 +0400

    first

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="py-[20px] text-[18px] text-center text-white">
-      © By Bunyad Mirzazada 2023
+      &#169; By Bunyad Mirzazada 2023
     </div>
   );
 }
